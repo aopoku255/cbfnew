577d88ec73c720ea34f388a0718d8c5f1ac74545
aopoku255 <aopoku255@gmail.com>
2024-11-23 13:16:49 +0000

    Fixing Not found

--- api/api.js
+++ api/api.js
@@ -3,39 +3,36 @@ const path = require("path");
 
 const app = express();
 
-// Serve static files from both folders
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../previous")));
 
-// Dynamic route for pages
 app.get("/:page", (req, res, next) => {
-  const publicFile = path.join(
-    __dirname,
-    "../public",
-    `${req.params.page}.html`
-  );
-  const previousFile = path.join(
-    __dirname,
-    "../previous",
-    `${req.params.page}.html`
-  );
-
-  // Check the public folder first
-  res.sendFile(publicFile, (err) => {
+  const fileName = path.join(__dirname, "../public", `${req.params.page}.html`);
+  res.sendFile(fileName, (err) => {
     if (err) {
-      // If not in public, check the previous folder
-      res.sendFile(previousFile, (errPrevious) => {
-        if (errPrevious) {
-          next(); // Proceed to 404 if not found
-        }
-      });
+      next();
     }
   });
 });
 
-// 404 handler
+app.get("/:2024", (req, res, next) => {
+  const fileName = path.join(
+    __dirname,
+    "../previous",
+    `${req.params.page}.html`
+  );
+  res.sendFile(fileName, (err) => {
+    if (err) {
+      next();
+    }
+  });
+});
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "../public", "404.html"));
+});
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "../previous", "404.html"));
 });
 
 module.exports = app;
