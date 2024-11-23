a0aeada8a93630aed0e8a3012bcdecf56f0fc2f0
aopoku255 <aopoku255@gmail.com>
2024-11-23 13:13:10 +0000

    Fixing Not found

--- api/api.js
+++ api/api.js
@@ -3,17 +3,37 @@ const path = require("path");
 
 const app = express();
 
+// Serve static files from both folders
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../previous")));
 
+// Dynamic route for pages
 app.get("/:page", (req, res, next) => {
-  const fileName = path.join(__dirname, "../public", `${req.params.page}.html`);
-  res.sendFile(fileName, (err) => {
+  const publicFile = path.join(
+    __dirname,
+    "../public",
+    `${req.params.page}.html`
+  );
+  const previousFile = path.join(
+    __dirname,
+    "../previous",
+    `${req.params.page}.html`
+  );
+
+  // Check the public folder first
+  res.sendFile(publicFile, (err) => {
     if (err) {
-      next();
+      // If not in public, check the previous folder
+      res.sendFile(previousFile, (errPrevious) => {
+        if (errPrevious) {
+          next(); // Proceed to 404 if not found
+        }
+      });
     }
   });
 });
 
+// 404 handler
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, "../public", "404.html"));
 });
